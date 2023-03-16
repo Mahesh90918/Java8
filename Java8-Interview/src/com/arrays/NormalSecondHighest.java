509a5e6df21f99c3a57e1c35545ef5221206fc61
Mahesh90918 <mahesh996345@gmail.com>
2023-03-16 14:11:51 +0530

    normal second

---
+++ Java8-Interview/src/com/arrays/NormalSecondHighest.java
@@ -0,0 +1,21 @@
+package com.arrays;
+
+public class NormalSecondHighest {
+	public static void main(String[] args) {
+		int[] arr = { 12, 45, 32, 43, 23, 12, 45 };
+		int largeNumber = 0;
+		int secondNumber = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > largeNumber) {
+				secondNumber = largeNumber;
+				largeNumber = arr[i];
+			} else if (arr[i] > secondNumber && arr[i] !=largeNumber) {
+				secondNumber = arr[i];
+				
+			}
+		}
+		System.out.println(largeNumber);
+		System.out.println(secondNumber);
+	}
+}
