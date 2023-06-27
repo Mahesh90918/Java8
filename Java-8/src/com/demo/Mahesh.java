e6a8fe1f16f1ad10b54047908559c8de0e54c324
Mahesh90918 <112085942+Mahesh90918@users.noreply.github.com>
2023-06-27 15:55:36 +0530

    Update Mahesh.java

--- Java-8/src/com/demo/Mahesh.java
+++ Java-8/src/com/demo/Mahesh.java
@@ -4,6 +4,9 @@ public class Mahesh {
 	
 	public static void main(String[] args) {
 		System.out.println("Mahesh");
+		System.out.println("Mahesh");
+		System.out.println("Mahesh");
+		System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");System.out.println("Mahesh");
 	}
 
 }
